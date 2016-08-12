09d3a5eabe9a32499a59664175d6a8a02e79f68a
Swojeet K <swojeetk@swojeet>
2016-08-12 15:26:32 +0545

    Added editing and updateing feature

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,6 +8,20 @@ class IdeasController < ApplicationController
 		redirect_to root_path
 	end
 
+	def edit
+		@idea= Idea.find(params[:id])
+	end
+
+	def update
+		@idea = Idea.find(params[:id])
+		if @idea.update(idea_params)
+			redirect_to root_path
+		else
+			redirect_to edit_idea_path(params[:id])
+		end
+	end
+
+
 	private
 	def ideas_params
 		params.require(:idea).permit(:description, :author)
