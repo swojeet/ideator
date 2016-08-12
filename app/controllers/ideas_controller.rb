185f721d0f9421c917ecdb2a5f89f5f596659d52
Swojeet K <swojeetk@swojeet>
2016-08-12 09:19:11 +0545

    Form created

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,14 @@ class IdeasController < ApplicationController
 	def index
 		@ideas= Idea.all
 	end
+
+	def create
+		@idea=Idea.create(ideas_params)
+		redirect_to root_path
+	end
+
+	private
+	def ideas_params
+		params.require(:idea).permit(:description, :author)
+	end
 end
